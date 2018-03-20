2276f30cd639ae3fbd96d65141c63c837a249efe
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-20 04:35:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 katz_deli = []
-line_numbers = Array.new
+
+
 
 def line(array)
   puts "The line is currently empty." if array.size == 0
-  puts "The line is currently:" 
+  puts "The line is currently:"
 end
 
 def take_a_number(array,new_person)
   array.push(new_person)
-  line_numbers.push(array.index(new_person)+1)
   return "Welcome, #{new_person}. You are number {(array.index(new_person)+1)} in line."
 end
