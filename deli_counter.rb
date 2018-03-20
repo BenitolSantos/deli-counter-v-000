5b2ca856206bcf6f3ba3ca184381d27c613f8ac4
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-20 04:19:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ katz_deli = []
 
 def line(array)
   puts "The line is currently empty." if array.size == 0
-  puts "The line is currently: " + array.each_with_index.split()
+  line_numbers
+  puts "The line is currently:
 end
 
 def take_a_number(array,new_person)
