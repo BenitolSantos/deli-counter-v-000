4740a447326806827781ac0b10266ed9f98cb48d
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-20 04:22:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ line_numbers = Array.new
 
 def line(array)
   puts "The line is currently empty." if array.size == 0
-  
   puts "The line is currently:"
 end
 
 def take_a_number(array,new_person)
   array.push(new_person)
+  array.index(new_person)
   return "Welcome, #{new_person}. You are number {(array.index(new_person)+1)} in line."
 end
