ba16f6216056c2f12bb693e73a703e826244abac
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-20 03:54:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,5 +7,5 @@ end
 
 def take_a_number(array,new_person)
   array.push(new_person)
-  return "Welcome, #{new_person}. You are number 3 in line."
+  return "Welcome, #{new_person}. You are number {(array.index(new_person)+1)} in line."
 end
